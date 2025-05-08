a0ea9d538d59ed4bf107cf67343f44f5175b8631
Aldo Flores <sarduts@gmail.com>
2025-05-08 11:59:03 -0600

    crea la clase 'Menu'

---
+++ src/Menu.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Menu {
+    private HashMap<String, String> coleccionDivisas = new HashMap<>();
+    private HashMap<Integer, String> listaDeOpciones = new HashMap<>();
+    private int opcionesNumero = 3;
+
+    public HashMap<String, String> getColeccionDivisas() {
+        return coleccionDivisas;
+    }
+
+    public HashMap<Integer, String> getListaDeOpciones() {
+        return listaDeOpciones;
+    }
+
+    public int getOpcionesNumero() {
+        opcionesNumero = listaDeOpciones.size() + 1;
+
+        if(coleccionDivisas.size() == 0){
+            opcionesNumero = 1;
+        }
+        return opcionesNumero;
+    }
+
+    public void agregarDivisa(String divisa, String nombreDivisa){
+        coleccionDivisas.put(divisa, nombreDivisa);
+    }
+
+    public void quitarDivisa(String divisa){
+        coleccionDivisas.remove(divisa);
+    }
+
+    public void generarMenu(String divisaPrincipal){
+        String menuDivisas = """
+                *************************************************
+                Sea bienvenido/a al Conversor de Mondedas  =)
+                """;
+        //List<Object> soloDivisas = Arrays.stream(coleccionDivisas.keySet().toArray()).toList();
+        int numeral = 0;
+
+        for (String value : coleccionDivisas.keySet()) {
+            if (value != divisaPrincipal){
+            numeral ++;
+            menuDivisas += "\n  " + numeral + ") " + coleccionDivisas.get(divisaPrincipal) + " =>> " + coleccionDivisas.get(value);
+            numeral ++;
+            menuDivisas += "\n  " + numeral + ") " + coleccionDivisas.get(value) + " =>> " + coleccionDivisas.get(divisaPrincipal);
+            listaDeOpciones.put(numeral - 1, value);
+            listaDeOpciones.put(numeral, value);
+
+            }
+        }
+        numeral++;
+        menuDivisas += "\n  " + numeral + ") Salir";
+        System.out.println(menuDivisas);
+
+    }
+
+}
