d3bba451c42d1591c762e2ec3ec7fac0067f3479
Aldo Flores <sarduts@gmail.com>
2025-05-07 21:07:33 -0600

    captura los datos en un record

--- src/DatosApi.java
+++ src/DatosApi.java
@@ -8,10 +8,11 @@ import java.net.http.HttpResponse;
 
 public class DatosApi {
     private String infoJson;
-    private Gson gson;
+    private Gson gson = new Gson();
     private HttpClient client = HttpClient.newHttpClient();
     private HttpRequest request;
     private HttpResponse<String> response;
+    private DatasApi datas;
 
     public DatosApi(String desde, String para) throws IOException, InterruptedException {
         Direccion url = new Direccion(desde);
@@ -22,7 +23,10 @@ public class DatosApi {
         response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         infoJson = response.body();
-        System.out.println(infoJson);
+        //System.out.println(infoJson);
+
+        datas = (gson.fromJson(infoJson, DatasApi.class));
+        //System.out.println(datas);
     }
 
 }
