e110b9d95aeb55be53b1a82fb98a56e0814ada19
Aldo Flores <sarduts@gmail.com>
2025-05-07 19:47:43 -0600

    crea la clase 'Direccion'

---
+++ src/Direccion.java
@@ -0,0 +1,15 @@
+public class Direccion {
+    private String urlOrigin = "https://v6.exchangerate-api.com/v6/35b3ba3cc1f9f011a619d682/latest/";
+    private String URLnueva;
+
+    public String getURLnueva() {
+        return URLnueva;
+    }
+
+    public Direccion(String busqueda){
+        URLnueva = urlOrigin + busqueda;
+        System.out.println(URLnueva);
+    }
+
+
+}
