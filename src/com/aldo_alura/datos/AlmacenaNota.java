e95a482d4ca0ea0363834bd981a3b8b54280c7b0
Aldo Flores <sarduts@gmail.com>
2025-05-09 20:19:56 -0600

    crea método 'actualizaLista'

--- src/com/aldo_alura/datos/AlmacenaNota.java
+++ src/com/aldo_alura/datos/AlmacenaNota.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AlmacenaNota {
-    private List<NotaData> listaData =new ArrayList<>();
+    private List<NotaData> listaData = new ArrayList<>();
+
+    public void actualizaLista(List<NotaData> listaData) {
+        this.listaData = listaData;
+    }
 
     public void  agrega(NotaData notaData){
         NotaData nuevaNota = new NotaData(notaData);
